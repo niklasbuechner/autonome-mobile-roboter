fee466b017cc8fdd33a560fbd6575606ac5bfb17
Jörg Vierling <dev@vierling.cloud>
2023-07-25 20:39:32 +0200

    Add mindet for code

--- document/latex/create.cmd
+++ document/latex/create.cmd
@@ -1,15 +1,16 @@
+pip install Pygments
 @echo off
 SET RESULT_DIR=..\result
-SET OUTPUT_DIR=..\temp
+SET OUTPUT_DIR=temp
 SET FILE=thesis
 cd tex
 mkdir %RESULT_DIR%
 mkdir %OUTPUT_DIR%
-pdflatex -output-directory=%OUTPUT_DIR% -synctex=1 %FILE%.tex
+pdflatex  -output-directory=%OUTPUT_DIR% -synctex=1 -shell-escape  %FILE%.tex
 biber %OUTPUT_DIR%\%FILE%
 makeindex %OUTPUT_DIR%\%FILE%.idx
 makeglossaries %OUTPUT_DIR%\%FILE%
-pdflatex -output-directory=%OUTPUT_DIR% -synctex=1 %FILE%.tex
-pdflatex -output-directory=%OUTPUT_DIR% -synctex=2 %FILE%.tex
+pdflatex -output-directory=%OUTPUT_DIR% -synctex=1 -shell-escape %FILE%.tex
+pdflatex -output-directory=%OUTPUT_DIR% -synctex=2 -shell-escape %FILE%.tex
 copy %OUTPUT_DIR%\%FILE%.pdf %RESULT_DIR%\%FILE%.pdf
 cd ..
--- document/latex/create.sh
+++ document/latex/create.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 RESULT_DIR="../result"
-OUTPUT_DIR="../temp"
+OUTPUT_DIR="temp"
 FILE="thesis"
 cd tex
 mkdir $RESULT_DIR
 mkdir $OUTPUT_DIR
 cp literatur.bib $OUTPUT_DIR
-pdflatex --shell-escape -output-directory="$OUTPUT_DIR" -synctex=1 -interaction=nonstopmode ${FILE}.tex
+pdflatex -output-directory="$OUTPUT_DIR" -synctex=1 -shell-escape -interaction=nonstopmode ${FILE}.tex
 cd $OUTPUT_DIR
 biber ${FILE}
 makeindex ${FILE}.idx
 makeglossaries ${FILE}
 cd ../tex
-pdflatex --shell-escape -output-directory="$OUTPUT_DIR" -synctex=1 -interaction=nonstopmode ${FILE}.tex
-pdflatex --shell-escape -output-directory="$OUTPUT_DIR" -synctex=2 -interaction=nonstopmode ${FILE}.tex
+pdflatex -output-directory="$OUTPUT_DIR" -synctex=1 -shell-escape -interaction=nonstopmode ${FILE}.tex
+pdflatex -output-directory="$OUTPUT_DIR" -synctex=2 -shell-escape -interaction=nonstopmode ${FILE}.tex
 cat $OUTPUT_DIR/${FILE}.pdf > $RESULT_DIR/${FILE}.pdf
 cd ..
