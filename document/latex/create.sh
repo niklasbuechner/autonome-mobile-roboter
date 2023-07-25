8050f190970468ff7ddd2d9d1cebd43413cf0fa6
Niklas Büchner <code@niklasbuechner.com>
2023-07-25 18:35:37 +0200

    Software Feedback Teil einarbeiten

--- document/latex/create.sh
+++ document/latex/create.sh
@@ -6,13 +6,13 @@ cd tex
 mkdir $RESULT_DIR
 mkdir $OUTPUT_DIR
 cp literatur.bib $OUTPUT_DIR
-pdflatex -output-directory="$OUTPUT_DIR" -synctex=1 -interaction=nonstopmode ${FILE}.tex
+pdflatex --shell-escape -output-directory="$OUTPUT_DIR" -synctex=1 -interaction=nonstopmode ${FILE}.tex
 cd $OUTPUT_DIR
 biber ${FILE}
 makeindex ${FILE}.idx
 makeglossaries ${FILE}
 cd ../tex
-pdflatex -output-directory="$OUTPUT_DIR" -synctex=1 -interaction=nonstopmode ${FILE}.tex
-pdflatex -output-directory="$OUTPUT_DIR" -synctex=2 -interaction=nonstopmode ${FILE}.tex
+pdflatex --shell-escape -output-directory="$OUTPUT_DIR" -synctex=1 -interaction=nonstopmode ${FILE}.tex
+pdflatex --shell-escape -output-directory="$OUTPUT_DIR" -synctex=2 -interaction=nonstopmode ${FILE}.tex
 cat $OUTPUT_DIR/${FILE}.pdf > $RESULT_DIR/${FILE}.pdf
 cd ..
